3a9995fcf7688c4865c2f1ec79611588b353d1d1
Kevin Hernandez <kmhs-18@hotmail.com>
2023-03-15 19:30:39 -0300

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Kevin Hernandez on 15/03/23.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
